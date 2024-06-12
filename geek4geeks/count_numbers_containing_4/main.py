457bf9eb449aa3afba96c8daf3702cd54a75b300
Chris Fraher <fraher@gmail.com>
2024-06-11 23:12:01 -0500

    Added Count Numbers with 4

---
+++ geek4geeks/count_numbers_containing_4/main.py
@@ -0,0 +1,57 @@
+
+
+
+import json
+
+class Solution:
+    def countNumberswith4(self, n : int) -> int:
+        # code here
+        if n<4:
+            return 0
+        elif n==4:
+            return 1
+        else:
+            total = 0
+            
+            for i in range(n):
+                i+=1
+                for j in str(i):
+                    if int(j) == 4:
+                        total+=1
+                        break
+        
+            return total
+
+
+
+
+if __name__ == "__main__":
+
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+
+    correct = 0
+    incorrect = 0
+
+    errors = list()
+
+    for sample in data['samples']:
+
+        n = sample['n']
+
+        obj = Solution()
+        res = obj.countNumberswith4(n)
+
+        if res == sample['count']:
+            correct+=1
+        else:
+            incorrect+=1
+            sample['yours'] = res
+            errors.append(sample)
+    
+    print('Total Correct: ', correct)
+    print('Total Incorrect: ', incorrect)
+
+    if incorrect > 0:
+        print('Incorrect Items:')
+        print(errors)
